b6a73befec9e6bb89cf626cbc917945496e7dd7a
Sam <samuel.furneaux@gmail.com>
2026-02-03 14:18:21 +0000

    Improving Dylans code

--- cleaning.py
+++ cleaning.py
@@ -1,3 +1,6 @@
 import pandas as pd
 df = pd.read_csv('Forward_Feast_Survey.csv')
 print(df.count()/len(df)*100) 
+
+## Dropping column "4b1. And are you satisfied with what you have?" as on 15% of data is there
+df = df.drop(columns='4b1. And are you satisfied with what you have?')
